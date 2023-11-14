24ebf8a8b5eb3bc1482f6f62024067804f3f7e22
Vipan Kumar <vipan.webdeveloper@gmail.com>
2023-11-14 17:44:56 +0530

    Added Shop Information page

--- Pixelpro/Helloworld/Block/Link.php
+++ Pixelpro/Helloworld/Block/Link.php
@@ -12,7 +12,7 @@ class Link extends \Magento\Framework\View\Element\Html\Link
 protected function _toHtml()
     {
      if (false != $this->getTemplate()) {
-     return parent::_toHtml();
+        return parent::_toHtml();
      }
      return '<li><a ' . $this->getLinkAttributes() . ' >' . $this->escapeHtml($this->getLabel()) . '</a></li>';
     }
